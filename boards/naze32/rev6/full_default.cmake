109f1b8e89e52f0c9d7bf49662618cf898ff0135
Christian Rauch <Rauch.Christian@gmx.de>
2018-12-05 01:24:34 +0000

    enable more modules

--- boards/naze32/rev6/full_default.cmake
+++ boards/naze32/rev6/full_default.cmake
@@ -44,58 +44,56 @@ px4_add_board(
 		#vmount
 
 	MODULES
-		# attitude_estimator_q
+		attitude_estimator_q
 		#camera_feedback
-		# commander
-		# dataman
-		# ekf2
-		# events
+		commander
+		dataman
+		ekf2
+		events
 		#fw_att_control
 		#fw_pos_control_l1
 		#gnd_att_control
 		#gnd_pos_control
-		#gpio_led
-		# land_detector
-		# landing_target_estimator
-		# load_mon	# needs CONFIG_SCHED_INSTRUMENTATION
-		# local_position_estimator
-		# logger	# watchdog support requires
-		# mavlink
-		# micrortps_bridge
-		# mc_att_control	# requires mc_pos_control, sensors
-		# mc_pos_control	# requires navigator
-		# navigator	# requires mc_pos_control, commander
+		land_detector
+		landing_target_estimator
+		load_mon
+		local_position_estimator
+		# logger
+		mavlink
+		mc_att_control
+		mc_pos_control
+		navigator
 		#position_estimator_inav
-		# sensors
-		#uavcan
+		sensors
+		#vmount
 		#vtol_att_control
 		#wind_estimator
 
-	# SYSTEMCMDS
-	# 	#bl_update
-	# 	config
-	# 	dumpfile
-	# 	esc_calib
-	# 	# hardfault_log	# needs SRAM on this STM32
-	# 	led_control
-	# 	mixer
-	# 	#motor_ramp
-	# 	motor_test
-	# 	#mtd
-	# 	nshterm
-	# 	param
-	# 	perf
-	# 	pwm
-	# 	reboot
-	# 	reflect
-	# 	sd_bench
-	# 	shutdown
-	# 	#tests # tests and test runner
-	# 	top
-	# 	topic_listener
-	# 	tune_control
-	# 	usb_connected
-	# 	ver
+	SYSTEMCMDS
+		#bl_update
+		config
+		dumpfile
+		esc_calib
+		# hardfault_log
+		led_control
+		mixer
+		#motor_ramp
+		motor_test
+		#mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		#tests # tests and test runner
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
 
 	EXAMPLES
 		#bottle_drop # OBC challenge
