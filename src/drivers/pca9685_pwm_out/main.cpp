684deecc595cc100640822245d3a88bb762773a3
Christian Rauch <Rauch.Christian@gmx.de>
2023-08-03 23:05:07 +0200

    assign valid character device for PCA9685Wrapper

--- src/drivers/pca9685_pwm_out/main.cpp
+++ src/drivers/pca9685_pwm_out/main.cpp
@@ -121,7 +121,7 @@ protected:
 };
 
 PCA9685Wrapper::PCA9685Wrapper(int schd_rate_limit) :
-	CDev(nullptr),
+	CDev("/dev/pwm_out"),
 	OutputModuleInterface(MODULE_NAME, px4::wq_configurations::hp_default),
 	_cycle_perf(perf_alloc(PC_ELAPSED, MODULE_NAME": cycle")),
 	_schd_rate_limit(schd_rate_limit)
